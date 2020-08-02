6b33c6bff608fc9c33cb7a8f2cce75d1b0c002c5
USIS002320 <68787485+USIS002320@users.noreply.github.com>
2020-08-02 15:47:39 -0600

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim nTabla, i As SByte
+        nTabla = txtNtabla.Text
+
+        lstTabla.Items.Clear()
+
+        If nTabla <= 12 Then
+            For i = 1 To 10
+                lstTabla.Items.Add(nTabla.ToString() + "X" + i.ToString() + "=" + (nTabla * i).ToString())
+
+            Next
+        Else
+            lstTabla.Items.Add("por favor ingrese solo tablas menores o iguales a 12.")
+
+        End If
+
+
+    End Sub
+End Class
