d2cb22bf71e71959d1ef29375506661241d5ab13
Billy Josue Villanueva Abrego <usis002320@ugb.edu.sv>
2020-08-23 12:15:02 -0600

    Subiendo Convertidor de Dinero

---
+++ HOLAmundo/Convertidor de Dinero/Convertidor de Dinero/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cbxsalida = New System.Windows.Forms.ComboBox()
+        Me.cbxentrada = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblr = New System.Windows.Forms.Label()
+        Me.lblum = New System.Windows.Forms.Label()
+        Me.cmdconvertir = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtvalor = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cbxsalida
+        '
+        Me.cbxsalida.BackColor = System.Drawing.Color.MistyRose
+        Me.cbxsalida.FormattingEnabled = True
+        Me.cbxsalida.Items.AddRange(New Object() {"Peso mexicano", "Dolar", "Euro", "Colon Salvadoreño", "Sol peruano", "Bitcoin", "Cordoba", "Yen", "Quetzal", "Yuan"})
+        Me.cbxsalida.Location = New System.Drawing.Point(531, 204)
+        Me.cbxsalida.Name = "cbxsalida"
+        Me.cbxsalida.Size = New System.Drawing.Size(121, 21)
+        Me.cbxsalida.TabIndex = 0
+        '
+        'cbxentrada
+        '
+        Me.cbxentrada.BackColor = System.Drawing.Color.MistyRose
+        Me.cbxentrada.FormattingEnabled = True
+        Me.cbxentrada.Items.AddRange(New Object() {"Peso mexicano", "Dolar", "Euro", "Colon Salvadoreño", "Sol peruano", "Bitcoin", "Cordoba", "Yen", "Quetzal", "Yuan"})
+        Me.cbxentrada.Location = New System.Drawing.Point(531, 136)
+        Me.cbxentrada.Name = "cbxentrada"
+        Me.cbxentrada.Size = New System.Drawing.Size(121, 21)
+        Me.cbxentrada.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(265, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(243, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Convertidor de Dinero"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(77, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(158, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Escriba el Valor"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(469, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(253, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Seleccione la Moneda del Pais"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(494, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(27, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "De:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(501, 207)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(19, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "A:"
+        '
+        'lblr
+        '
+        Me.lblr.AutoSize = True
+        Me.lblr.Location = New System.Drawing.Point(85, 64)
+        Me.lblr.Name = "lblr"
+        Me.lblr.Size = New System.Drawing.Size(15, 15)
+        Me.lblr.TabIndex = 7
+        Me.lblr.Text = "0"
+        '
+        'lblum
+        '
+        Me.lblum.AutoSize = True
+        Me.lblum.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblum.Location = New System.Drawing.Point(63, 64)
+        Me.lblum.Name = "lblum"
+        Me.lblum.Size = New System.Drawing.Size(0, 16)
+        Me.lblum.TabIndex = 8
+        '
+        'cmdconvertir
+        '
+        Me.cmdconvertir.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.cmdconvertir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdconvertir.Location = New System.Drawing.Point(65, 158)
+        Me.cmdconvertir.Name = "cmdconvertir"
+        Me.cmdconvertir.Size = New System.Drawing.Size(184, 51)
+        Me.cmdconvertir.TabIndex = 9
+        Me.cmdconvertir.Text = "Convertir"
+        Me.cmdconvertir.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkSalmon
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(65, 215)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 162)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Borrar"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Tan
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(160, 215)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(89, 162)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'txtvalor
+        '
+        Me.txtvalor.Location = New System.Drawing.Point(65, 132)
+        Me.txtvalor.Name = "txtvalor"
+        Me.txtvalor.Size = New System.Drawing.Size(184, 20)
+        Me.txtvalor.TabIndex = 13
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox1.Controls.Add(Me.lblr)
+        Me.GroupBox1.Controls.Add(Me.lblum)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(452, 267)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Resultado"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtvalor)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.cmdconvertir)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbxentrada)
+        Me.Controls.Add(Me.cbxsalida)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cbxsalida As ComboBox
+    Friend WithEvents cbxentrada As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblr As Label
+    Friend WithEvents lblum As Label
+    Friend WithEvents cmdconvertir As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txtvalor As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ HOLAmundo/Convertidor de Dinero/Convertidor de Dinero/Form1.vb
@@ -0,0 +1,345 @@
+﻿Public Class Form1
+    Private Sub cmdconvertir_Click(sender As Object, e As EventArgs) Handles cmdconvertir.Click
+
+        If cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.046
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.039
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 0.4
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.16
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.0000039
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 1.59
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 4.81
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 0.35
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Peso mexicano" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 0.31
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Dolar" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 21.98
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.85
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 8.75
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 3.58
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.000086
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 34.84
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 105.8
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 7.7
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Dolar" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 6.91
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Euro" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 25.92
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 1.18
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 10.33
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 4.22
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.0001
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 41.1
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 124.79
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 9.09
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Euro" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 8.15
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 6.14
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.28
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 2.45
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.24
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.000024
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 9.74
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 29.57
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 2.15
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Sol peruano" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 1.93
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 2.51
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.11
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.097
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.41
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.0000098
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 3.98
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 12.09
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 0.88
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Colon Salvadoreño" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 0.79
+            lblum.Text = "¥"
+        End If
+
+
+        If cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 255738.9
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 11614.7
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 9847.14
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 101683.57
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 41580.9
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 404766.6
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 1229020.51
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 89494.93
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Bitcoin" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 80296.7
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.63
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.029
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.024
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 0.25
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.1
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.0000025
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 3.04
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 0.22
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Cordoba" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 0.2
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Yen" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.21
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.0095
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.08
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 0.83
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.034
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.00
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 0.33
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 0.073
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Yen" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 0.066
+            lblum.Text = "¥"
+        End If
+
+
+        If cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "Q"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 2.85
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.13
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.11
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 1.14
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.46
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.000011
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 4.52
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 13.74
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Quetzal" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text) * 0.9
+            lblum.Text = "¥"
+        End If
+
+        If cbxentrada.Text = "Yuan" And cbxsalida.Text = "Yuan" Then
+            lblr.Text = Val(txtvalor.Text)
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Peso mexicano" Then
+            lblr.Text = Val(txtvalor.Text) * 3.18
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Dolar" Then
+            lblr.Text = Val(txtvalor.Text) * 0.14
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Euro" Then
+            lblr.Text = Val(txtvalor.Text) * 0.12
+            lblum.Text = "€"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Colon Salvadoreño" Then
+            lblr.Text = Val(txtvalor.Text) * 1.27
+            lblum.Text = "$"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Sol peruano" Then
+            lblr.Text = Val(txtvalor.Text) * 0.52
+            lblum.Text = "S/"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Bitcoin" Then
+            lblr.Text = Val(txtvalor.Text) * 0.000012
+            lblum.Text = "฿"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Cordoba" Then
+            lblr.Text = Val(txtvalor.Text) * 5.04
+            lblum.Text = "C$"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Yen" Then
+            lblr.Text = Val(txtvalor.Text) * 15.31
+            lblum.Text = "¥"
+        ElseIf cbxentrada.Text = "Yuan" And cbxsalida.Text = "Quetzal" Then
+            lblr.Text = Val(txtvalor.Text) * 1.11
+            lblum.Text = "Q"
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        lblr.Text = ""
+        lblum.Text = ""
+        txtvalor.Text = ""
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        End
+
+    End Sub
+End Class
